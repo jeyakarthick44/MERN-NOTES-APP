684098900f149bfa4af2103ef0a58059cfdeeebd
jeyakarthick44 <jeyakarthick408@gmail.com>
2021-12-15 23:46:49 +0530

    next Redux

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,8 @@ import './bootstrap.min.css'
 import LandingPage from './LandingPage';
 import MyNotes from './MyNotes';
 import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
+import RegisterPage from './RegisterPage';
+import LoginScreen from './LoginScreen';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} exact />
         <Route path="/mynotes" element={<MyNotes />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterPage />} />
         </Routes>
         </Router>
     </div>
